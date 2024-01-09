9804ef77ce98efb32255095012f5c2440e89e769
Lincoln Bunker <lincolnbunker03@gmail.com>
2024-01-09 14:41:26 -0700

    Mars2

--- mars.js
+++ mars.js
@@ -17,3 +17,31 @@ if (excited === "Y") {
 if (excited === "N") {
     alert("Well, it's too late to back out now")
 }
+
+alert("It's time to pack for your trip to Mars.");
+let numSuitcases = prompt("How many suitcases do you plan to bring?");
+numSuitcases = Number(numSuitcases);
+
+if (numSuitcases > 2) {
+    alert("That's way too many cuh, you'll have to pack more lightly.");
+    alert("Please try to fit your stuff into 2 suitcases");
+}
+
+else { 
+    alert("Perfect! You finna fit in the ship!")
+}
+
+alert("You're allowed to bring one companion animal with you.");
+const companionType = prompt("What kind of animal would you like to bring?");
+let companionName = prompt("What is your companion's name?");
+
+let firstLetter = companionName [0];
+firstLetter = firstLetter.toUppercase();
+
+let otherLetters = companionName.slice(1);
+otherLetters= otherLetters.toLowerCase();
+
+companionName = firstLetter + otherLetters;
+
+
+alert(`Dope, so you bringing ${companionName} the ${companionType}`);
