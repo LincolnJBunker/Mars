a4225bae7332b3168605304b599d167543665bb5
Lincoln Bunker <lincolnbunker03@gmail.com>
2024-01-09 15:57:56 -0700

    Mars3

--- mars.js
+++ mars.js
@@ -11,10 +11,10 @@ alert('Yesterday, you received a call from your good friend at NASA');
 alert("They need someone to go to Mars this weekend, and YOU'VE been chosen!!");
 let excited = prompt("Are you excited? Type Y o N");
 excited = excited.toUpperCase();
-if (excited === "Y") {
+if (excited.startsWith("Y")) {
     alert("I knew you'd say that. It's so cool that you're going to Mars!")
 }
-if (excited === "N") {
+if (excited.startsWith("N")) {
     alert("Well, it's too late to back out now")
 }
 
